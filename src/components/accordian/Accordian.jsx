3900a0e4880e83f3c341569cd37be0b2aef43716
darrin@darrinduncan.com <darrin@darrinduncan.com>
2024-01-12 21:45:44 -0500

    Create multi selection button

--- src/components/accordian/Accordian.jsx
+++ src/components/accordian/Accordian.jsx
@@ -14,6 +14,8 @@ const Accordian = () => {
                 <h2>Frequently Asked Questions</h2>
                 <p className="content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, iure maxime quibusdam blanditiis aspernatur mollitia alias tempore accusantium placeat animi amet corporis quisquam ea assumenda ab soluta velit? Nemo, sequi molestias, suscipit ipsam fugiat impedit alias corrupti labore temporibus, quia ratione repudiandae aliquam. Voluptas vel nihil exercitationem, odio distinctio dolores.</p>
 
+                <button>Enable Multi Selection</button>
+
                 {
                     data && data.length > 0 ?
                         data.map((item, idx) => (
