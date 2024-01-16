0707c1bb7ffe3a6a19410701517f2d6682b48782
darrin@darrinduncan.com <darrin@darrinduncan.com>
2024-01-15 21:52:56 -0500

    Add hex color generator functionality

--- src/components/aside/index.jsx
+++ src/components/aside/index.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiGlobe, FiPackage } from "react-icons/fi";
 import './style.css'
 
 export const Menu = () => {
+  const [selected, setSelected] = useState(null)
   return (
     <div className="menu">
       <ul>
--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -2,11 +2,33 @@ import React, { useState } from 'react'
 import "./style.css"
 
 const RandomColor = () => {
-const [bucket, setBucket] = useState('Random Color')
+    const [typeOfColor, setTypeOfColor] = useState('hex')
+    const [color, setColor] = useState('#000000')
+
+    const randomColorUtility = (length) => {
+        return Math.floor(Math.random() * length)
+    }
+
+    const handleGenerateRandomHexColor = () => {
+        const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+        let hexColor = '#'
+
+        for (let i = 0; i < 6; i++) {
+            hexColor += hex[randomColorUtility(hex.length)]
+        }
+        setColor(hexColor)
+    }
+    const handleGenerateRandomRgbColor = () => {
+
+    }
+
     return (
-        <div className="randomColor">
+        <div className="randomColor" style={{ width: '100vw', height: '100vh', backgroundColor: color }}>
             <div className="container">
-            <h2>{bucket}</h2>
+                <h2>Random Color Generator</h2>
+                <button className="btn" onClick={() => setTypeOfColor('hex')}>Generate HEX Color</button>
+                <button className="btn" onClick={() => setTypeOfColor('rgb')}>Generate RGB Color</button>
+                <button className="btn" onClick={typeOfColor === 'hex' ? handleGenerateRandomHexColor : handleGenerateRandomRgbColor}>Generate Random Color</button>
             </div>
         </div>
     )
